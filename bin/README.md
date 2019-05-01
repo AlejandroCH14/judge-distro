c532aef88fd0b8935f7a7b6d6556ba846f1bf5e4
Daniel Sprohar <danielsprohar@outlook.com>
2019-05-01 12:47:49 -0500

    gui

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# Judge Distro
+A program to assign judges for the Intermediate Division of the Permain Basin Regional Science Fair
--- src/main/java/utpb/science/fair/App.java
+++ src/main/java/utpb/science/fair/App.java
@@ -10,7 +10,7 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
 		primaryStage.setTitle("Judge Scheduler");
 		primaryStage.setScene(new Scene(root, 650, 450));
 		primaryStage.show();
